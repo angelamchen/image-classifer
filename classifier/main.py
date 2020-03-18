db76633648946018c4dac09d698eb30ff6b3ecbb
angelachen <angelachen085@gmail.com>
2020-03-18 19:55:16 -0400

    Script to read csv and plot data of fortune500

---
+++ classifier/main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+(x_train,y_train), (x_test,y_test)= mnist.load_data()
+
+import matplotlib.pyplot as plt
+
+image_index = 7777 # You may select anything up to 60,000
+print(y_train[image_index]) # The label is 8
+plt.imshow(x_train[image_index], cmap='Greys')
